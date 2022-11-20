84429de73c67d3f243637625b32d9b38b17265cc
motasimmakki <makkimotasim@gmail.com>
2022-11-20 13:14:09 +0530

    :art:style: Form structure of login page

---
+++ pages/login/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '../../assets/Instagram.jpeg'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function index() {
+  return (
+    <div className='login-cont'>
+        <div className='login-card'>
+            <Image src={logo} alt="instagram-logo"/>
+            <TextField id="outlined-basic" label="E-mail"
+             variant="outlined" fullWidth margin="dense"/>
+            <TextField id="outlined-basic" label="Password" 
+            variant="outlined" fullWidth margin="dense"/>
+            <div className='forget-password'>
+                Forget Password?
+            </div>
+            <Button variant="contained" fullWidth margin="dense"
+            className='login-btn'>
+                Log In
+            </Button>
+        </div>
+        <div className='bottom-card'>
+            Don't have an account? <span className='signup-btn-in-login'>Signup</span>
+        </div>
+    </div>
+  )
+}
