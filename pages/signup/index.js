dec249a556f0813020db59528079abfead0beb6d
motasimmakki <makkimotasim@gmail.com>
2022-11-20 11:56:47 +0530

    :art:style: Partial structure of signup page

---
+++ pages/signup/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '../../assets/Instagram.jpeg'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CloudUploadTwoToneIcon from '@mui/icons-material/CloudUploadTwoTone';
+
+export default function index() {
+  return (
+    <div className='signup-cont'>
+        <div className='signup-card'>
+            <Image src={logo} alt="instagram-logo"/>
+            <TextField id="outlined-basic" label="E-mail"
+             variant="outlined" fullWidth margin="dense"/>
+            <TextField id="outlined-basic" label="Password" 
+            variant="outlined" fullWidth margin="dense"/>
+            <TextField id="outlined-basic" label="Full Name" 
+            variant="outlined" fullWidth margin="dense"/>
+            <Button variant="outlined" fullWidth margin="dense" 
+            color="secondary" className='upload-btn'>
+                <CloudUploadTwoToneIcon className='upload-icon'/>
+                Upload Profile Image
+            </Button>
+        </div>
+    </div>
+  )
+}
