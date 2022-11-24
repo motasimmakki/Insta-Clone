3393f89366f5b31a453366251f008ddb18a22334
motasimmakki <makkimotasim@gmail.com>
2022-11-24 22:59:06 +0530

    :technologist:feat: Store profile pic in firebase storage

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import CloudUploadTwoToneIcon from '@mui/icons-material/CloudUploadTwoTone';
 import Link from 'next/link';
 import { AuthContext } from '../../context/auth';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 export default function Index() {
     const [email, setEmail] = useState("");
@@ -25,7 +26,29 @@ export default function Index() {
             setLoading(true);
             setError("");
             const userInfo = await signup(email, password);
-            console.log("User Signed In!");
+            
+            const storage = getStorage();
+            // Upload file and metadata to the object 'images/mountains.jpg'
+            const storageRef = ref(storage, `${userInfo.user.uid}/Profile`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.on('state_changed',
+            (snapshot) => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+            }, 
+            (error) => {
+                console.log(error);
+            }, 
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                });
+                console.log("User Signed In!");
+            })
         } catch(err) {
             console.log("Error: ", err);
             setError(err.code);
